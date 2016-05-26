f8f1d0736bb003807d812074b36c1c85fdfcac50
chudziutkaa <klaudiachudy94@gmail.com>
2016-05-27 00:25:07 +0200

    Implement avg_notes method in student_decorator

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -4,5 +4,11 @@ class StudentDecorator < BaseDecorator
   end
 
   def avg_notes(subject_item)
+    if subject_item_notes.present?
+  		avg = subject_item_notes.for_subject_item(subject_item).average(:value)
+  		"#{avg.round(2)}"
+    else
+      "0"
+    end
   end
 end
