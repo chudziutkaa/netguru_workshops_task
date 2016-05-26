69130b6ebaad3235242214240dd440937f9f6020
chudziutkaa <klaudiachudy94@gmail.com>
2016-05-27 00:28:02 +0200

    Add full_name_with_title method for TeacherDecorator

--- app/decorators/teacher_decorator.rb
+++ app/decorators/teacher_decorator.rb
@@ -1,4 +1,8 @@
 class TeacherDecorator < BaseDecorator
+	def full_name_with_title
+		"#{firs_name} #{last_name} [#{academic_title}]"
+	end
+
   def full_name
     "#{first_name} #{last_name}"
   end
