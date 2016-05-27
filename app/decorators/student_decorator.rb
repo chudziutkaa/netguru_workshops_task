77a2f9d1613ec3ff0774d1eddd772f5bea8a435e
chudziutkaa <klaudiachudy94@gmail.com>
2016-05-27 11:41:37 +0200

    Change avg_notes in StudentDecorator
    return float

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -4,11 +4,7 @@ class StudentDecorator < BaseDecorator
   end
 
   def avg_notes(subject_item)
-    if subject_item_notes.present?
   		avg = subject_item_notes.for_subject_item(subject_item).average(:value)
-  		"#{avg.round(2)}"
-    else
-      "0"
-    end
+  		"%.2f"  %(avg || 0)
   end
 end
